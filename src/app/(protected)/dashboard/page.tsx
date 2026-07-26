1adf6ad1ec732a28e35a29779dd75b054c70f382
Adrish <runakarak@gmail.com>
2026-07-26 12:07:03 +0530

    tt

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -3,12 +3,40 @@
 import React from 'react'
 import { useUser } from '@clerk/nextjs'
 import useProject from '@/hooks/use-project'
+import { FiGithub } from "react-icons/fi";
+import Link from 'next/link';
+import { ExternalLink } from 'lucide-react';
 
 const page = () => {
     const { user } = useUser()
     const { project } = useProject()
     return (
-        <div>{project?.name}</div>
+        <div>
+            <div className='flex items-center justify-between flex-wrap gap-y-4'>
+                {/* Linked Repo */}
+                <div className="w-fit rounded-md bg-primary px-4 py-3">
+                    <div className="flex items-center">
+                        <FiGithub className='size-5 text-white' />
+                        <div className='ml-2'>
+                            <p className='text-sm font-medium text-white'>
+                                This Project is Linke to {' '}
+                                <Link href={project?.githubUrl ?? ""} className='inline-flex items-center text-white/80 hover:underline'>
+                                    {project?.githubUrl}
+                                    <ExternalLink className='ml-1 size-4' />
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="h-4"></div>
+                <div className='flex items-center gap-4'>
+                    TeamMembers
+                    Invitation
+                    Archivebuttuon
+                </div>
+            </div>
+        </div>
     )
 }
 
